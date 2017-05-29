9b051ef82e8bcd4ba95f199d52174c98849d67ee
infoflow <infoflow@163.com>
2017-05-29 23:56:48 +0800

    递归解法

--- 198/198_House_Robber.cpp
+++ 198/198_House_Robber.cpp
@@ -2,20 +2,26 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+using namespace std;
 class Solution {
 	public :
 		int solve(int idx,vector<int>& nums){
 			if(idx < 0){
 				return 0;	
 			}	
+			return max(nums[idx]+solve(idx-2,nums),solve(idx-1,nums));
 		}		
 		int rob(vector<int>& nums){
-
+			return solve(nums.size()-1,nums);
 		}
-}
+};
 int main(){
-
-
-
+	Solution s;
+	vector<int> a;
+	a.push_back(1);
+	a.push_back(2);
+	a.push_back(3);
+	cout<<s.rob(a)<<endl;
 return 0;
 }
