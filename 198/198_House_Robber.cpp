52190faec97983d49a93e1ebbb8d65a04c798c0c
infoflow <infoflow@163.com>
2017-05-29 23:27:34 +0800

    create the file

---
+++ 198/198_House_Robber.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+class Solution {
+	public :
+		int solve(int idx,vector<int>& nums){
+			if(idx < 0){
+				return 0;	
+			}	
+		}		
+		int rob(vector<int>& nums){
+
+		}
+}
+int main(){
+
+
+
+return 0;
+}
